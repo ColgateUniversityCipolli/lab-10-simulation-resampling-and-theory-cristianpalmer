b560572f5667e3d4b132d335056d5ac3e2fb7576
cristianpalmer <cpalmer@colgate.edu>
2025-04-03 15:16:27 -0400

    starting task 2

--- Lab10WorkingR.R
+++ Lab10WorkingR.R
@@ -24,8 +24,8 @@ ggplot(data = data_simulation_1, aes(x = x)) +
 
 # What is the range of the middle 95%?
 
-lower_bound <- quantile(data_simulation$x, 0.025) 
-upper_bound <- quantile(data_simulation$x, 0.975) 
+lower_bound <- quantile(data_simulation_1$x, 0.025) 
+upper_bound <- quantile(data_simulation_1$x, 0.975) 
 middle_range <- upper_bound - lower_bound
 
 # Range of middle 95% is ~0.06
@@ -41,7 +41,7 @@ margin_of_error = middle_range/2
 
 # Task 1 Part 2: Repeat Simulation for Double Sample Size 
 
-data_simulation_2 <- tibble(x=rbinom(10000, size = 1004*2, prob = 0.39)) /1004
+data_simulation_2 <- tibble(x=rbinom(10000, size = 1004*2, prob = 0.39)) /(1004*2)
 
 # Plot a histogram of the data
 ggplot(data = data_simulation_2, aes(x = x)) +
@@ -68,10 +68,19 @@ middle_range_2 <- upper_bound_2 - lower_bound_2
 
 margin_of_error_2 = middle_range_2/2
 
-# Margin of error is ~4.3
+# Margin of error is ~2%
 
-# This margin of error of ~3% is better than Gallup's margin of error of 4%
+# This margin of error of ~2% is about equal to Gallup's margin of error of 2%
 
 
 
 ####################################################################################################
+# Task 2: Resampling
+
+# Create a df containing data from Gallup Survey
+
+gallup_data <- tibble(1004)
+
+
+
+
